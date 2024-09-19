7461eb5d7803e8706d769d6b3b6dd7f78500fc04
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-09-19 05:44:21 -0700

    drawing on canvas

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-return (
-  <></>
+  const canvasRef = useRef(null);
+  const [drawing, setDrawing] = useState(false);
+
+
+  const handleMouseDown = (e)=>{
+    setDrawing(true);
+    console.log(`mouse pos ${e.clientX}, ${e.clientY}`);
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(e.clientX-canvas.offsetLeft, e.clientY-canvas.offsetTop);
+  }
+
+  const handleMouseMove = (e)=>{
+    if (!drawing){
+      return
+    }
+
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    ctx.lineTo(e.clientX-canvas.offsetLeft, e.clientY-canvas.offsetTop);
+    ctx.stroke();
+  }
+
+  const handleMouseUp = ()=>{
+    setDrawing(false);
+  }
+
+  return (
+   <div>
+    <canvas ref={canvasRef} width="800px" height="800px"  onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}/>
+   </div>
   )
 }
 
