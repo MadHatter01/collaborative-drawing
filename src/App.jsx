8d7c783b3bbd5ffc2afc6e523a4940cf987663e3
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-09-20 16:07:43 -0700

    brush initial color and type

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const canvasRef = useRef(null);
   const [drawing, setDrawing] = useState(false);
   const [brushSize, setBrushSize] = useState(4);
-  
+  const [brushType, setBrushType] = useState('solid');
 
 
   const handleMouseDown = (e)=>{
@@ -26,8 +26,18 @@ function App() {
 
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
+    if (brushType === 'solid'){
+      ctx.setLineDash([]);
+    }
+    else if(brushType === 'dashed'){
+      ctx.setLineDash([15,15]);
+    }
+    else if(brushType === 'dotted'){
+      ctx.setLineDash([2,8]);
+    }
     ctx.lineWidth = brushSize;
     ctx.lineTo(e.clientX-canvas.offsetLeft, e.clientY-canvas.offsetTop);
+    ctx.strokeStyle = '#ffffff'
     ctx.stroke();
   }
 
@@ -39,11 +49,20 @@ function App() {
     setBrushSize(Number(e.target.value));
   }
 
+  const handleBrushType = (e)=>{
+    setBrushType(e.target.value);
+  }
   return (
   
   <div className='container'>
+
    <div className='canvasActions'>
-    <label htmlFor={brushSize}>Brush Size</label><input type="range" id="brushSize" name="brushSize" min="1" max="50" value={brushSize} onChange={handleBrushSize} />
+    <div className='form-element'>
+    <label htmlFor={brushSize}>Brush Size</label><input type="range" id="brushSize" name="brushSize" min="1" max="50" value={brushSize} onChange={handleBrushSize} /><span>{brushSize}</span>
+    </div>
+    <div className='form-element'>
+    <label htmlFor={brushType}>Brush Type</label><select id="brushType" name="brushType" value={brushType} onChange={handleBrushType}> <option value="solid">Solid</option><option value="dashed">Dashed</option><option value="dotted">Dotted</option></select>
+    </div>
     </div>
     <canvas ref={canvasRef} width="800px" height="800px"  onMouseDown={handleMouseDown} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}/>
    </div>
